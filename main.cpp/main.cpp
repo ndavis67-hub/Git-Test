6781eb4884afb03ba50820350371b82f172760d7
Nasir Davis <ndavis67@ut.utm.edu>
2026-01-23 21:48:28 -0600

    Product of git_test

--- main.cpp/main.cpp
+++ main.cpp/main.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int sum (int n);
+int product (int n);
+
 int main(){
 
   int n;
@@ -9,18 +11,34 @@ int main(){
   cout << "Give me a number: ";
   cin >> n;
 
-  int final = sum(n); 
+  int final = sum(n);
+
+  int prod = product(n);
 
   cout << "The sum from 1 to " << n << " is " << final << endl;
+
+  cout << "The product from 1 to " << n << " is " << prod << endl;
   return 0;
 
 }
 
 int sum (int n){
+  
   int total = 0;
 
   for (int i = 1; i <= n; i++){
     total += i;
   }
   return total;
+}
+
+int product (int n){
+  
+  int result = 1;
+
+  for (int i = 1; i<= n; i++){
+    result *= i;
+  }
+
+  return result;
 }
