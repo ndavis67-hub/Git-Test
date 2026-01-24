b717ad43000bc2d8b3faee1f9974919c17a42053
Nasir Davis <ndavis67@ut.utm.edu>
2026-01-23 20:11:05 -0600

    Added user input and function to get the sum

--- main.cpp/main.cpp
+++ main.cpp/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int sum (int n);
 int main(){
-  cout << "Hello World!" << endl;
 
+  int n;
+
+  cout << "Give me a number: ";
+  cin >> n;
+
+  int final = sum(n); 
+
+  cout << "The sum from 1 to " << n << " is " << final << endl;
   return 0;
 
+}
+
+int sum (int n){
+  int total = 0;
+
+  for (int i = 1; i <= n; i++){
+    total += i;
+  }
+  return total;
 }
